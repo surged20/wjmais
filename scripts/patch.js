aadb819e59dc4ca2565828a355098ce3e24d1b5b
Surge <surged20@gmail.com>
2022-07-21 17:27:38 -0400

    Update data.data and data.flags references for v10
    Updates all instances of:
      - data.data -> system
      - data.flags -> flags

--- scripts/patch.js
+++ scripts/patch.js
@@ -2,10 +2,10 @@ function patchCompendiumImport() {
   // Override default system token bar values on wildjammer import from compendium.
   // We want token bars from the compendium to be used for ships to pick up BP.
   libWrapper.register('wjmais', 'WorldCollection.prototype.fromCompendium', function (wrapped, ...args) {
-    if (!args[0].data.flags?.wjmais?.speed)
+    if (!args[0].flags?.wjmais?.speed)
       return wrapped(...args);
 
-    const token = args[0].data.token;
+    const token = args[0].token;
     const data = wrapped(...args);
     data.token.bar1 = token.bar1;
     data.token.bar2 = token.bar2;
@@ -16,7 +16,7 @@ function patchCompendiumImport() {
 function patchItemSheet() {
   // Display wildjammer modules and upgrades as mountable items like vehicle equipment
   libWrapper.register('wjmais', 'game.dnd5e.applications.ItemSheet5e.prototype._isItemMountable', function (wrapped, ...args) {
-    const armorType = this.document.data.data?.armor?.type;
+    const armorType = this.document.system?.armor?.type;
     const wjEquipmentTypes = ["foremantle", "material", "modifier", "module", "upgrade"];
     return wrapped(...args) || wjEquipmentTypes.includes(armorType);
   }, 'MIXED' );
@@ -37,10 +37,10 @@ function patchResourceBars() {
   libWrapper.register('wjmais', 'TokenDocument.prototype.getBarAttribute', function (wrapped, ...args) {
     const barName = args[0];
     const alternative = args[1]?.alternative;
-    const attr = alternative || (barName ? this.data[barName].attribute : null);
+    const attr = alternative || (barName ? this[barName].attribute : null);
     if (attr === "Bulwark Points") {
-      const value = foundry.utils.getProperty(this.actor.data.data, "attributes.hp.temp");
-      const max = foundry.utils.getProperty(this.actor.data.data, "attributes.hp.tempmax");
+      const value = foundry.utils.getProperty(this.actor.system, "attributes.hp.temp");
+      const max = foundry.utils.getProperty(this.actor.system, "attributes.hp.tempmax");
       const model = game.system.model.Actor[this.actor.type];
       return {
         type: "bar",
@@ -61,12 +61,12 @@ function patchResourceBars() {
     const isDelta = args[2];
 
     if (attribute === "Bulwark Points") {
-      const currentValue = foundry.utils.getProperty(this.data.data, "attributes.hp.temp");
-      const currentMax = foundry.utils.getProperty(this.data.data, "attributes.hp.tempmax");
+      const currentValue = foundry.utils.getProperty(this.system, "attributes.hp.temp");
+      const currentMax = foundry.utils.getProperty(this.system, "attributes.hp.tempmax");
 
       // Determine the updates to make to the actor data
       if (isDelta) value = Math.clamped(0, Number(currentValue) + value, currentMax);
-      const updates = {[`data.attributes.hp.temp`]: value};
+      const updates = {[`system.attributes.hp.temp`]: value};
 
       return this.update(updates);
     }
@@ -75,29 +75,29 @@ function patchResourceBars() {
   }, 'MIXED' );
 
   // Display raw HP bar for wildjammers
-  libWrapper.register('wjmais', 'game.dnd5e.entities.Token5e.prototype._drawBar', function (wrapped, ...args) {
-    if (game.actors.get(this.data.actorId).data.flags?.wjmais?.traits)
-      return Object.getPrototypeOf(game.dnd5e.entities.Token5e).prototype._drawBar.apply(this, args);
+  libWrapper.register('wjmais', 'game.dnd5e.canvas.Token5e.prototype._drawBar', function (wrapped, ...args) {
+    if (game.actors.get(this.document.actorId).flags?.wjmais?.traits)
+      return Object.getPrototypeOf(game.dnd5e.canvas.Token5e).prototype._drawBar.apply(this, args);
     else
       return wrapped(...args);
   }, 'MIXED' );
 
   // Display raw HP value for wildjammers
-  libWrapper.register('wjmais', 'game.dnd5e.entities.TokenDocument5e.prototype.getBarAttribute', function (wrapped, ...args) {
-    if (game.actors.get(this.data.actorId).data.flags?.wjmais?.traits)
-      return Object.getPrototypeOf(game.dnd5e.entities.TokenDocument5e).prototype.getBarAttribute.apply(this, args);
+  libWrapper.register('wjmais', 'game.dnd5e.documents.TokenDocument5e.prototype.getBarAttribute', function (wrapped, ...args) {
+    if (game.actors.get(this.actorId).flags?.wjmais?.traits)
+      return Object.getPrototypeOf(game.dnd5e.documents.TokenDocument5e).prototype.getBarAttribute.apply(this, args);
     else
       return wrapped(...args);
   }, 'MIXED' );
 }
 
 function patchRollData() {
-  libWrapper.register('wjmais', 'game.dnd5e.entities.Actor5e.prototype.getRollData', function (wrapped, ...args) {
-    const ship = game?.actors?.get(this.data.flags?.wjmais?.shipId);
-    const size = ship ? ship.data.data.traits.size : this.data.data.traits.size;
+  libWrapper.register('wjmais', 'game.dnd5e.documents.Actor5e.prototype.getRollData', function (wrapped, ...args) {
+    const ship = game?.actors?.get(this.flags?.wjmais?.shipId);
+    const size = ship ? ship.system.traits.size : this.system.traits.size;
 
     // Add @ship.ac.mod and @ship.ram.dice formula support
-    this.data.data["ship"] = {
+    this.system["ship"] = {
       "ac": {"mod": CONFIG.WJMAIS.shipModifiers[size].ac},
       "ram": {"dice": CONFIG.WJMAIS.shipRamDice[size]}
     };
@@ -108,7 +108,7 @@ function patchRollData() {
 
 function patchProficiency() {
   libWrapper.register('wjmais', 'CONFIG.Actor.documentClass.prototype._prepareVehicleData', function (wrapped, ...args) {
-    if (!args[0].flags?.wjmais?.npc)
+    if (this.flags?.wjmais?.npc)
       wrapped(...args);
   }, 'MIXED' );
 }
