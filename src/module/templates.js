d9b21f16a84a3b261603055d58e4174b181c5018
Surge <surged20@gmail.com>
2024-06-06 07:11:57 -0400

    Rename partials to .hbs and register slugified names

--- src/module/templates.js
+++ src/module/templates.js
@@ -4,9 +4,18 @@
  * @return {Promise}
  */
 export const preloadHandlebarsTemplates = async function () {
-  return loadTemplates([
+
+  const partials = [
     // Actor Sheet Partials
-    "modules/wjmais/templates/actors/parts/actor-features.html",
-    "modules/wjmais/templates/actors/parts/bridge-crew-roles.html",
-  ]);
+    "modules/wjmais/templates/actors/parts/actor-features.hbs",
+    "modules/wjmais/templates/actors/parts/bridge-crew-roles.hbs",
+  ];
+
+  const paths = {};
+  for ( const path of partials ) {
+    paths[path.replace(".hbs", ".html")] = path;    
+    paths[`wjmais.${path.split("/").pop().replace(".hbs", "")}`] = path;
+  }
+
+  return loadTemplates(paths);
 };
